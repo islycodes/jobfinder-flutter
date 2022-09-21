1d868e8ef49edcff54d1fc6a0df1d2eb9734bdba
Adrielly Isly <islyg22@gmail.com>
2022-09-21 14:14:55 -0300

    mudança nas preferencias

--- lib/view/tela_inicial.dart
+++ lib/view/tela_inicial.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'inicial_widget.dart';
 
@@ -8,69 +10,60 @@ class TelaInicial extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color(0xfffbf4ff),
-      body: Column(
-        children:[
-            Row(
+        body: Column(children: [
+          Row(
+            children: [
+              InicialWidget('image1.png'),
+            ],
+          ),
+
+          //Título
+          Container(
+            height: 40,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                InicialWidget('image1.png'),
+                Text(
+                  'Estagiotec',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(154, 151, 162, 10),
+                  ),
+                )
               ],
             ),
-
- //Título
-           Container(
-             height: 40,
-             child: Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: [
-                 Text(
-                   'Estagiotec',
-                   style: TextStyle(
-                     fontSize: 36,
-                     fontWeight: FontWeight.bold,
-                     color: Color.fromRGBO(154, 151, 162, 10),
-                   ),
-                 )
-               ],
-             ),
-           ),
+          ),
 
 //Subtitulo
-           Container(
-             height: 50,
-             child: Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: [
-                 Text(
-                   'Criado para alunos por alunos',
-                style: TextStyle(
-                )
-                 )
-               ],
-             ),
-           ),
-
-//Subtitulo
-           Container(
-             height: 60,
-             child: Row(
-               mainAxisAlignment: MainAxisAlignment.center,
-               children: [
-                 Text(
-                    'Explore as melhores vagas baseadas\n'),
-                 + 'oi'),
-               ],
-             ),
-           ),
-
-//Subtitulo
-           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          Container(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text('no seu interesse e área de estudo'),
-             ],
-           ),
-        ]
-      )
-    );
+                Text('Criado para alunos por alunos', style: TextStyle())
+              ],
+            ),
+          ),
+
+//Subtitulo
+          Container(
+            height: 60,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Explore as melhores vagas baseadas\n'),
+              ],
+            ),
+          ),
+
+//Subtitulo
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('no seu interesse e área de estudo'),
+            ],
+          ),
+        ]));
   }
 }
