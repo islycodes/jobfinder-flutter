85a628cc380585e3e1e7e3ae7c0e66373042a094
Adrielly Isly <islyg22@gmail.com>
2022-09-28 22:50:33 -0300

    Update tela_Otp.dart

--- lib/view/Auth/tela_Otp.dart
+++ lib/view/Auth/tela_Otp.dart
@@ -1,16 +1,33 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class TelaOtp extends StatefulWidget {
-  const TelaOtp({super.key});
+class OtpForm extends StatelessWidget {
+  const OtpForm({Key? key}) : super(key: key);
 
-  @override
-  State<TelaOtp> createState() => _TelaOtpState();
-}
-
-class _TelaOtpState extends State<TelaOtp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.white,
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new, color: Colors.grey),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ),
+        body: Form(
+          child: Row(
+            children: [
+              SizedBox(
+                height: 68,
+                width: 64,
+                child: TextField(),
+              )
+            ],
+          ),
+        ));
   }
 }
