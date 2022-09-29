4ed14d5741cde7ec3bbffdd4511ebfdeffbcfa8f
Felipe Gabriel Botelho <felipeg.bot@gmail.com>
2022-09-29 16:38:39 -0300

    Update tela_menu_inicial.dart

--- lib/view/tela_menu_inicial.dart
+++ lib/view/tela_menu_inicial.dart
@@ -7,60 +7,92 @@ class TelaMenuInicial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = TextEditingController();
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+      body: SingleChildScrollView(
+          padding: EdgeInsets.all(20),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: MediaQuery.of(context).size.height,
+            ),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.account_circle, size: 40, color: Colors.black),
-                  Padding(
-                    padding: EdgeInsets.only(left: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Nome do usuário',
-                            style:
-                                TextStyle(fontSize: 18, color: Colors.black)),
-                        Text('Curso do usuário - período',
-                            style:
-                                TextStyle(fontSize: 12, color: Colors.black)),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-              Text('Encontre seu estágio',
-                  style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold)),
-              Row(
-                children: [
-                  Container(
-                    color: Color.fromARGB(255, 8, 209, 8),
-                    height: 74,
-                    width: 268,
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 10, right: 10),
-                      child: campoTexto('rotulo', null),
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(Icons.account_circle, size: 40, color: Colors.black),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Nome do usuário',
+                                style: TextStyle(
+                                    fontSize: 18, color: Colors.black)),
+                            Text('Curso do usuário - período',
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.black)),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
-                  Container(
-                    height: 74,
-                    width: 74,
-                    color: Color.fromRGBO(84, 51, 99, 1),
-                    child: Icon(Icons.settings_input_composite_outlined,
-                        size: 40, color: Colors.white),
+                  Text('Encontre seu estágio',
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold)),
+                  Row(
+                    children: <Widget>[
+                      Flexible(
+                        child: campoTexto('Pesquisar', controller),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Icon(Icons.filter_alt_outlined))
+                    ],
                   ),
-                ],
-              )
-            ]),
-      ),
+                  Text('Vagas em destaque',
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold)),
+                  Text('Relacionado ao seu curso',
+                      style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold)),
+                  // ['asd','kj','asd','asdasd'].map((item) {
+                  //   return Container(child: Text(item));
+                  // }).toList(),
+                  Column(
+                    children: [
+                      'asd',
+                      'asd',
+                      'asdas',
+                      '',
+                      ',',
+                      ',',
+                      ',',
+                      ',',
+                      ',',
+                      '',
+                      'asd'
+                    ].map((e) {
+                      return Card(
+                        shadowColor: Colors.black54,
+                        elevation: 5,
+                        child: ListTile(
+                          title: Text(e),
+                          subtitle: Text('Capital: ${e}'),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ]),
+          )),
     );
   }
 }
