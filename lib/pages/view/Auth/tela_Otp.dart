7bd5175f6d86da4dc73d268003380f4537a7d2f2
Adrielly Isly <islyg22@gmail.com>
2022-10-04 15:44:13 -0300

    Update tela_Otp.dart

--- lib/pages/view/Auth/tela_Otp.dart
+++ lib/pages/view/Auth/tela_Otp.dart
@@ -11,146 +11,145 @@ class TelaOtp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new, color: Colors.grey),
-          onPressed: () => Navigator.of(context).pop(),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new, color: Colors.grey),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
         ),
-      ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(30, 0, 30, 30),
-        child: Form(
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(30, 0, 30, 30),
+            child: Form(
+              child: Column(
                 children: [
-                  SizedBox(
-                      width: 300,
-                      height: 300,
-                      child:
-                          Image.asset("lib/image/verification-code-icon.png")),
-                ],
-              ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Código de verificação",
-                        style: TextStyle(
-                          color: Color.fromRGBO(85, 97, 250, 1),
-                          fontSize: 24,
-                          fontFamily: "Open Sans",
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Row(children: [
-                    Text(
-                        'Nós enviamos um código de verificação para\no seu e-mail.',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                        )),
-                  ]),
-                  SizedBox(
-                    height: 20,
-                  ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       SizedBox(
-                          height: 68,
-                          width: 64,
-                          child: TextFormField(
-                            onChanged: (value) {
-                              if (value.length == 1) {
-                                FocusScope.of(context).nextFocus();
-                              }
-                            },
-                            style: Theme.of(context).textTheme.headline6,
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            inputFormatters: [
-                              LengthLimitingTextInputFormatter(1),
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                          )),
-                      SizedBox(
-                          height: 68,
-                          width: 64,
-                          child: TextFormField(
-                            onChanged: (value) {
-                              if (value.length == 1) {
-                                FocusScope.of(context).nextFocus();
-                              }
-                            },
-                            style: Theme.of(context).textTheme.headline6,
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            inputFormatters: [
-                              LengthLimitingTextInputFormatter(1),
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                          )),
-                      SizedBox(
-                          height: 68,
-                          width: 64,
-                          child: TextFormField(
-                            onChanged: (value) {
-                              if (value.length == 1) {
-                                FocusScope.of(context).nextFocus();
-                              }
-                            },
-                            style: Theme.of(context).textTheme.headline6,
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            inputFormatters: [
-                              LengthLimitingTextInputFormatter(1),
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                          )),
-                      SizedBox(
-                          
-                          height: 68,
-                          width: 64,
-                          child: TextFormField(
-                            onChanged: (value) {
-                              if (value.length == 1) {
-                                FocusScope.of(context).nextFocus();
-                              }
-                            },
-                            style: Theme.of(context).textTheme.headline6,
-                            
-                            keyboardType: TextInputType.number,
-                            textAlign: TextAlign.center,
-                            inputFormatters: [
-                              LengthLimitingTextInputFormatter(1),
-                              FilteringTextInputFormatter.digitsOnly,
-                            ],
-                          ))
+                          width: 300,
+                          height: 300,
+                          child: Image.asset(
+                              "lib/image/verification-code-icon.png")),
                     ],
                   ),
-                  SizedBox(
-                    height: 20,
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Código de verificação",
+                            style: TextStyle(
+                              color: Color.fromRGBO(85, 97, 250, 1),
+                              fontSize: 24,
+                              fontFamily: "Open Sans",
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(children: [
+                        Text(
+                            'Nós enviamos um código de verificação para\no seu e-mail.',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            )),
+                      ]),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          SizedBox(
+                              height: 68,
+                              width: 64,
+                              child: TextFormField(
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                style: Theme.of(context).textTheme.headline6,
+                                keyboardType: TextInputType.number,
+                                textAlign: TextAlign.center,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly,
+                                ],
+                              )),
+                          SizedBox(
+                              height: 68,
+                              width: 64,
+                              child: TextFormField(
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                style: Theme.of(context).textTheme.headline6,
+                                keyboardType: TextInputType.number,
+                                textAlign: TextAlign.center,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly,
+                                ],
+                              )),
+                          SizedBox(
+                              height: 68,
+                              width: 64,
+                              child: TextFormField(
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                style: Theme.of(context).textTheme.headline6,
+                                keyboardType: TextInputType.number,
+                                textAlign: TextAlign.center,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly,
+                                ],
+                              )),
+                          SizedBox(
+                              height: 68,
+                              width: 64,
+                              child: TextFormField(
+                                onChanged: (value) {
+                                  if (value.length == 1) {
+                                    FocusScope.of(context).nextFocus();
+                                  }
+                                },
+                                style: Theme.of(context).textTheme.headline6,
+                                keyboardType: TextInputType.number,
+                                textAlign: TextAlign.center,
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(1),
+                                  FilteringTextInputFormatter.digitsOnly,
+                                ],
+                              ))
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      elevatedButtonGenerator(
+                          context, 'Confirmar', 'telaMudarSenha'),
+                    ],
                   ),
-                  elevatedButtonGenerator(
-                      context, 'Confirmar', 'telaMudarSenha'),
                 ],
               ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
