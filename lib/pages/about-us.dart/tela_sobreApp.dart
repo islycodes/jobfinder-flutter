3855b5f4f871683aeba5fb5bb621a2246031cf17
Adrielly Isly <islyg22@gmail.com>
2022-10-10 00:16:46 -0300

    Update tela_sobreApp.dart

--- lib/pages/about-us.dart/tela_sobreApp.dart
+++ lib/pages/about-us.dart/tela_sobreApp.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TelaSobreoAplicativo extends StatefulWidget {
   const TelaSobreoAplicativo({super.key});
 
@@ -12,21 +11,42 @@ class _TelaSobreoAplicativoState extends State<TelaSobreoAplicativo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      extendBodyBehindAppBar: false,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('SOBRE O APLICATIVO'),
-        titleTextStyle: TextStyle(
-            fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),
         backgroundColor: Colors.white,
-        elevation: 0,
-        toolbarHeight: 80,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new, color: Colors.grey),
-          onPressed: () => Navigator.of(context).pop(),
+        extendBodyBehindAppBar: false,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('SOBRE O APLICATIVO'),
+          titleTextStyle: TextStyle(
+              fontSize: 14, color: Colors.black, fontWeight: FontWeight.bold),
+          backgroundColor: Colors.white,
+          elevation: 0,
+          toolbarHeight: 80,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios_new, color: Colors.grey),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+            child: Container(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                            'Este projeto desenvolvido a partir da identificação de uma carência de softwares que auxiliem a conexão do estudante que busca estágio e da empresa que necessita de empregados. A partir disso foi pensado a criação deste aplicativo para ajudar o aluno a encontrar as vagas de estágio ofertadas pelas empresas na área de tecnologia da informação. Este portal foi desenvolvido em Dart usando o framework flutter. Novas atualizações e maior descrição do projeto pode ser encontrado',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.black,
+                            )),
+                        new InkWell(
+                            child: new Text('Open Browser'),
+                            onTap: () =>
+                                ('https://docs.flutter.io/flutter/services/UrlLauncher-class.html')),
+                      ],
+                    )
+                  ],
+                ))));
   }
 }
